4a6b3b30e9e37961dde35834d174da0b9de96ef3
Ashish <ashish.dutta050@gmail.com>
2017-01-31 08:39:14 +0000

    Mongoose db layer

---
+++ mongodb.js
@@ -0,0 +1,57 @@
+//Importing mongodb and mongoose modules
+var mongodb = require('mongodb');
+var mongoose = require("mongoose");
+var url = 'mongodb://localhost:27017/urlShortener';
+
+//Making mongodb connection from mongoose ODM
+var dbconn = mongoose.connect(url, function(error) {
+	if (error) {
+		return console.log("Error while trying to connect to mongodb ", error);
+	}
+	else {
+		console.log("Connection made to mongo db on local");
+	}
+});
+
+//Create a schema(object) -- a predefined structure/format for the documents in the 'urlshortener' collection.
+//Schema are the blueprint(class) for the documents in the collection.
+var urlShortnerSchema = mongoose.Schema({
+	originalLongUrl: String,
+	shortCode: {
+		type: Number,
+		index: true
+	}
+});
+
+//Models are constructors compiled from Schema definitions.
+//Different documents are instances of Model.All documents creation and retrieval from database are handled by models.
+//Creating a model(object) for querying,creating and validating documents in 'UrlData' collection based on the specific schema(urlShortnerSchema).
+
+var urlShortenerModel = dbconn.model('urldata', urlShortnerSchema);
+
+//module.exports object contains those contents of the file that we want other files to use
+module.exports = {
+	enterNewUrlData: function(newUrlData) {
+
+		/*var newEntry = new urlShortenerModel({
+			originalLongUrl: 'test------www.google.com-------test',
+			shortCode: 0
+		});*/
+
+		//New instance of model to enter new document
+		var newEntry = new urlShortenerModel(newUrlData);
+
+		//Document entry into db
+		newEntry.save(function(error) {
+			if (error) {
+				return console.log("An error has occured: ", error);
+			}
+			else {
+				console.log("Entry saved to collection in db");
+			}
+		});
+	}
+}
+
+
+//On enetring a document not following the Schema, a validation error will be thrown
--- server.js
+++ server.js
@@ -2,10 +2,42 @@
 var express = require("express");
 //Express server instance
 var app = express();
+//require() returns module.exports{} object of the mentioned js 
+var dbops = require('./mongodb.js');
 var port = process.env.PORT || 3000;
 
+//Express server object receiving a long url as a parameter
+app.get('/new/*', function(request, response) {
+    request.on('error', function(error) {
+        return console.log("An error has occurred ", error);
+    });
+    response.on('error', function(error) {
+        return console.log("An error has occurred ", error);
+    });
+
+    //Extract input Url from request stream
+    var longUrl = request.params[0];
+    
+    //Validate Url format
+    validateInputUrl(longUrl);
+    
+});
+
+function validateInputUrl(url){
+    
+}
+
+
+//test data for now
+var urlData = {
+    originalLongUrl: 'test1------www.google.com-------1test',
+    shortCode: 0
+};
+
+//Enter new url data to mongodb
+dbops.enterNewUrlData(urlData);
 
 //Express server instance listening on specific port
 app.listen(port, function() {
-	console.log("Server listening on port ", port);
+    console.log("Server listening on port ", port);
 });
